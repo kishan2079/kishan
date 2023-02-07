40cd88943732c749c53dfdc0e4a17c29f4fe1b2c
kishan2079 <121547047+kishan2079@users.noreply.github.com>
2023-02-07 22:56:19 +0530

    Create automate.py

---
+++ automate.py
@@ -0,0 +1,6 @@
+# Import webbrowser module in the program  
+import webbrowser  
+# Add a URL of JavaTpoint to open it in a browser  
+url= 'https://www.javatpoint.com/python-tutorial'  
+# Open the URL using open() function of module  
+webbrowser.open_new_tab(url)
